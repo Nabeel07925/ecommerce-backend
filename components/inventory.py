940e8f3e1d772934f7a2bcc0f677691c092d43b0
Nabeel <muhammad.nabeel38@yahoo.com>
2023-10-04 04:04:33 +0500

    add get by id functionality in inventory

--- components/inventory.py
+++ components/inventory.py
@@ -8,6 +8,10 @@ class InventoryComponent:
         return InventoryRepository.get_inventories(db)
 
     @staticmethod
+    def get_inventory_by_id(id_, db):
+        return InventoryRepository.get_inventory_by_id(id_, db)
+
+    @staticmethod
     def create_inventory(inventory: schema.Inventory, db):
 
         return InventoryRepository.create_inventory(inventory_info=inventory, db=db)
--- routers/inventory.py
+++ routers/inventory.py
@@ -19,6 +19,11 @@ async def get_all_inventory(db: Session = Depends(get_db)):
     return InventoryComponent.get_inventories(db)
 
 
+@router.get('/{inventory_id}')
+async def get_all_inventory(inventory_id, db: Session = Depends(get_db)):
+    return InventoryComponent.get_inventory_by_id(inventory_id, db)
+
+
 @router.post('/')
 async def create_inventory(request: schema.Inventory, db: Session = Depends(get_db)):
     try:
@@ -28,7 +33,7 @@ async def create_inventory(request: schema.Inventory, db: Session = Depends(get_
 
 
 @router.put('/{inventory_id}')
-async def create_inventory(inventory_id: int, request: schema.Inventory, db: Session = Depends(get_db)):
+async def update_inventory(inventory_id: int, request: schema.Inventory, db: Session = Depends(get_db)):
     try:
         return InventoryComponent.update_inventory(inventory_id=inventory_id,
                                                    inventory=request, db=db)
